d9c4878e8b60b7f231912f0bcac3cfe0d3df1e6c
ParkhomenkoDmitriy <mazzzarahha@gmail.com>
2024-04-29 13:39:50 +0300

    Перенесено в родительский класс self.move()         load()         self.show_info()         stop()

--- HW22.py
+++ HW22.py
@@ -1,5 +1,17 @@
 import time
 
+
+def load():
+    time.sleep(1)
+    print("load")
+    time.sleep(1)
+
+
+def stop():
+    print("stop")
+    time.sleep(1)
+
+
 class Auto:
     def __init__(self, brand, age, mark, color=None, weight=None):
         self.brand = brand
@@ -8,14 +20,19 @@ class Auto:
         self.color = color
         self.weight = weight
 
+    def perform_actions(self):
+        self.move()
+        load()
+        self.show_info()
+        stop()
+
     def move(self):
         print("move")
 
-    def stop(self):
-        print("stop")
+    def show_info(self):
+        print(f"Brand = {self.brand}, Age = {self.age}, Mark = {self.mark}, Color = {self.color}, "
+              f"Weight = {self.weight}")
 
-    def birthday(self):
-        self.age += 1
 
 class Truck(Auto):
     def __init__(self, brand, age, mark, max_load, color=None, weight=None):
@@ -26,10 +43,10 @@ class Truck(Auto):
         print("attention")
         super().move()
 
-    def load(self):
-        time.sleep(1)
-        print("load")
-        time.sleep(1)
+    def show_info(self):
+        print(f"Truck: Brand = {self.brand}, Age = {self.age}, Max Load = {self.max_load}, Color = {self.color}, "
+              f"Weight = {self.weight}")
+
 
 class Car(Auto):
     def __init__(self, brand, age, mark, max_speed, color=None, weight=None):
@@ -40,21 +57,19 @@ class Car(Auto):
         super().move()
         print(f"max speed is {self.max_speed}")
 
+    def show_info(self):
+        print(f"Car: Brand = {self.brand}, Age = {self.age}, Mark = {self.mark}, Max Speed = {self.max_speed},"
+              f" Color = {self.color}, Weight = {self.weight}")
+
+
 truck1 = Truck("Volvo", 5, "FH16", 40000, color="Blue", weight=12000)
 truck2 = Truck("Scania", 3, "R450", 45000, color="White", weight=12500)
 
 car1 = Car("BMW", 2, "320i", 250, color="Black", weight=1400)
 car2 = Car("Audi", 1, "A4", 240, color="Silver", weight=1450)
 
-print(f"Truck 1: Brand = {truck1.brand}, Age = {truck1.age}, Max Load = {truck1.max_load}")
-truck1.move()
-truck1.load()
+truck1.perform_actions()
+truck2.perform_actions()
 
-print(f"Truck 2: Brand = {truck2.brand}, Age = {truck2.age}, Max Load = {truck2.max_load}")
-truck2.move()
-
-print(f"Car 1: Brand = {car1.brand}, Age = {car1.age}, Max Speed = {car1.max_speed}")
-car1.move()
-
-print(f"Car 2: Brand = {car2.brand}, Age = {car2.age}, Max Speed = {car2.max_speed}")
-car2.move()
+car1.perform_actions()
+car2.perform_actions()
